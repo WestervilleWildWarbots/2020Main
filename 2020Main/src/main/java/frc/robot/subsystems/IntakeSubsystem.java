47069cb2ab15cc0c779f552730ba5813ce18fb7e
311993 <squirrellord19@gmail.com>
2020-01-11 14:47:18 -0500

    creates intake subsystem and commands, and initializes in robot

---
+++ 2020Main/src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -0,0 +1,20 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+import frc.robot.RobotMap;
+
+public class IntakeSubsystem extends Subsystem {
+
+
+  public IntakeSubsystem() {
+    
+    
+  } 
+
+  @Override
+  public void initDefaultCommand(){
+
+  }
+
+  
+}
